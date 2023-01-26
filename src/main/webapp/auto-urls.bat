7a62fd4ed5b09dac6da1f59b25622810a4de4da4
gwjgwj <gwjgwj@hanmail.net>
2023-01-26 22:50:35 +0900

    auto-urls.bat  - targetname에 배포대상 js 수정  - 추후 배포를 위한 로직 업데이트 가능

--- src/main/webapp/auto-urls.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd js/map
-set targetname="dtmap-urls.10.20.30.81.js"
-set ogname="dtmap-urls.js"
-Xcopy %targetname% %ogname% /y
-echo ===message===: %targetname% is copied to %ogname% :)
-cd ..
-pause
