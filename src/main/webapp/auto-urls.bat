2444c83386ce74877ab5d578192181fb56672ae6
gwjgwj <gwjgwj@hanmail.net>
2023-01-26 22:46:37 +0900

    autoconfig.bat  - targetname에

--- src/main/webapp/auto-urls.bat
+++ src/main/webapp/auto-urls.bat
@@ -4,6 +4,6 @@ cd js/map
 set targetname="dtmap-urls.10.20.30.81.js"
 set ogname="dtmap-urls.js"
 Xcopy %targetname% %ogname% /y
-echo ===message===: %targetname% 를 %ogname% 에 덮어쓰기 완료!
+echo ===message===: %targetname% is copied to %ogname% :)
 cd ..
 pause
