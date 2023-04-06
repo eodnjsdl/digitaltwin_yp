006b9cfe9f84baf6fc705c02194a908eb616bfbf
gwjgwj <gwjgwj@hanmail.net>
2023-04-06 10:24:43 +0900

    -

--- src/main/webapp/js/map/2d/map2d.js
+++ src/main/webapp/js/map/2d/map2d.js
@@ -2,9 +2,9 @@ window.map2d = (function () {
     /**
      * UTM-K
      */
-    proj4.defs("EPSG:5179", "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs");
+    proj4.defs("EPSG:5179", "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs ");
 
-    proj4.defs("EPSG:5174", "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs");
+    proj4.defs("EPSG:5174", "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43");
 
     /**
      * 중부원점 60만
